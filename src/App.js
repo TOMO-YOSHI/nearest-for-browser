27029381d9d848c394b6641015bf603945cd7d56
TOMO-YOSHI <lucky.paddy10@gmail.com>
2020-09-17 23:58:00 -0700

    implement google-maps with library

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import MyMapComponent from "./components/MyMapComponent/MyMapComponent.component.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyMapComponent
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+        isMarkerShown={true}
+      />
     </div>
   );
 }
